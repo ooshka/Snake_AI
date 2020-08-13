70a0a7847a9cb045a4577adf4c80a634e0535a1a
ooshka <69643010+ooshka@users.noreply.github.com>
2020-08-13 09:35:48 -0700

    Initial Git Upload
    Two basic files coded before git access including neural network code in Neural_V1.py and game code in Snake_V1.py

---
+++ Neural_V1.py
@@ -0,0 +1,252 @@
+"""
+ Neural Network Intro
+ 2020-08-02
+"""
+ 
+import sys
+import numpy as np
+import random
+import copy
+
+#Input Layer
+#First layer within the network responsible for taking inputs
+
+class Layer_Input:
+	#Forward Pass
+	#We will be ignoring training in this first case as the network will be evolving genetically
+	def forward(self, inputs):
+		self.output = inputs
+
+
+#Dense Layer
+#Basic Layer Structure
+class Layer_Dense:
+
+	#Layer Initialization
+	def __init__(self, n_inputs, n_neurons):
+		#Initialize both weights and biases
+		self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+
+		#This initializes the biases to zero.  We may not want this as the genetic algorithm has no way to alter the biases through mating
+		#self.biases = np.zeros((1, n_neurons))
+
+		self.biases = 0.01 * np.random.randn(1, n_neurons)
+
+	#Forward Pass Through Neuron
+	def forward(self, inputs):
+		#Calculate the basic output value of the neuron usings the dot product between weights and inputs and adding the bias
+		self.output = np.dot(inputs, self.weights) + self.biases
+
+#Rectified Linear Activaiton to be used within the hidden layers
+class Activation_ReLu:
+	#Forward Pass
+	def forward(self, inputs):
+		self.output = np.maximum(0, inputs)
+
+#Softmax Activation class to be used for output neurons
+class Activation_SoftMax:
+	#Forward Pass
+	def forward(self, inputs):
+		#Get unnormalized probabilities
+		exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+
+		#Normalize for each sample
+		probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+		self.output = probabilities
+
+#Overall Model Class that will house all pieces of the network
+class Model:
+
+	def __init__(self):
+		#Create a list of the objects in the network
+		self.layers = []
+		self.score = 0
+
+	#Add objects to the model
+	def add(self, layer):
+		self.layers.append(layer)
+
+	#In this initial case because the snake will be relying on a fitness function rather than a direct loss optimization we will be ignoring loss/optimizer settings
+
+	#Finalize the model
+	def finalize(self):
+
+		#Create and set the input layer
+		self.input_layer = Layer_Input()
+
+		#Count all the objects within the model
+		layer_count = len(self.layers)
+
+		#Initialize a list of the layers we want to train
+		self.trainable_layers = []
+
+		#Iterate through the model objects
+		for i in range(layer_count):
+			#If it's the first layer, the previous object will be the input layer
+			if i == 0:
+				self.layers[i].prev = self.input_layer
+				self.layers[i].next = self.layers[i+1]
+
+				#For all layers except the first and last
+			elif i < layer_count - 1:
+				self.layers[i].prev = self.layers[i-1]
+				self.layers[i].next = self.layers[i+1]
+
+			#Last layer
+			#Again in this case we are ignoring loss so the last layer will be the output of the network
+			else:
+				self.layers[i].prev = self.layers[i-1]
+				self.output_layer_activation = self.layers[i]
+
+			#Check to see if the layer is trainable (in our case genetically modifiable)
+			#Check for weights only is enough.  No need to look for biases
+			if hasattr(self.layers[i], 'weights'):
+				self.trainable_layers.append(self.layers[i])
+
+				#We are ignoring the loss aspect of the training b/c genetic
+
+	def mate(self, mom):
+		
+		child = copy.deepcopy(self)
+		for i in range(len(child.layers)):
+
+			if hasattr(child.layers[i], 'weights'):
+				
+				pass_on = bool(random.getrandbits(1))
+
+				if pass_on == True:
+					print("Male")
+					child.layers[i].weights = self.layers[i].weights
+					child.layers[i].biases = self.layers[i].biases
+				else:
+					print("Female")
+					child.layers[i].weights = mom.layers[i].weights
+					child.layers[i].biases = mom.layers[i].biases
+
+
+		child.score = 0
+
+		return child
+
+	#Perform the large forward pass of the model
+	def forward(self, X):
+
+		#Set the initial output of the first input layer of neurons y=X
+		self.input_layer.forward(X)
+
+		#Call the forward method of every object within the chain
+		for layer in self.layers:
+			layer.forward(layer.prev.output)
+
+		# 'layer' is now the last object in the list so we should return its output
+		#In our case this probably should be the probabilities of the choices
+		return layer.output
+
+
+def Model_Creator(self, n_inputs, n_neurons, n_outputs):
+
+	#Instantiate the model
+	model = Model()
+
+	#Add layers
+	model.add(Layer_Dense(n_inputs,n_neurons))
+	model.add(Activation_ReLu())
+	model.add(Layer_Dense(n_neurons,n_outputs))
+	model.add(Activation_SoftMax())
+
+	#Finalize the model
+	model.finalize()
+
+	return model
+
+#Define a super generation class to include a population of models
+class Generation:
+	def __init__(self, n_inputs, n_neurons, n_outputs, mating = True,  population_size = 100, threshold = 10):
+		
+		self.population_size = population_size
+		self.mating = mating
+		self.threshold = threshold
+
+		self.population = []
+		for i in range(population_size):
+			self.population.append(Model_Creator(self, n_inputs, n_neurons, n_outputs))
+
+	def score_sort(self):
+
+		self.population.sort(key = lambda x: x.score, reverse = True)
+
+	def generation_mate(self):
+
+		new_population = []
+
+		for i in range(self.population_size):
+			p1_indx = i % self.threshold
+			p2_indx = min(self.population_size - 1, int(np.random.exponential(self.threshold)))
+
+			offspring = self.population[p1_indx].mate(self.population[p2_indx])
+
+			new_population.append(offspring)
+
+		new_population[-1] = self.population[0]
+
+		self.population = new_population
+
+
+
+'''
+#Basic model testing with Sentdex data
+#Create the custom data set based on Sentdex course
+X, y = spiral_data(100, 3)
+
+#Instantiate the model
+model = Model()
+
+#Add layers
+model.add(Layer_Dense(2,512))
+model.add(Activation_ReLu())
+model.add(Layer_Dense(512,3))
+model.add(Activation_SoftMax())
+
+#Finalize the model
+model.finalize()
+
+#This should be placed within a "training algorithm"
+output = model.forward(X)
+
+'''
+
+
+'''
+
+#This is all the hardcoded testing that was done originally
+
+#Create a dense layer with inputs and outputs
+dense1 = Layer_Dense(num_inputs, num_outputs)
+
+dense2 = Layer_Dense(dense1.weights.shape[0] + 1, num_outputs)
+
+print(dense1.weights.shape[0])
+
+#Create activation object for hidden layers
+activation1 = Activation_ReLu()
+activation2 = Activation_SoftMax()
+
+#Make a forward pass with our training data
+dense1.forward(X)
+
+#Run initial neuron layer outputs through ReLu activation
+activation1.forward(dense1.output)
+
+#Run outputs of activaiton1 through the second hidden layer
+dense2.forward(activation1.output)
+
+#Run the outputs of the second hidden layer through the SofMax activation function
+activation2.forward(dense2.output)
+
+#Look at the first few samples
+print(activation2.output[:5])
+
+'''
+
+
---
+++ Snake_V1.py
@@ -0,0 +1,403 @@
+"""
+ Snake V0
+ 2020-07-22
+"""
+ 
+import sys
+import pygame
+import copy
+import numpy
+import random
+import Neural_V1 as nw
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+def draw_rect(x,y,color,size,b):
+
+    rect = pygame.Rect(x*size, y*size, size-b, size-b)
+    pygame.draw.rect(SCREEN, color, rect)
+
+def random_insert(Array, size):
+    
+    x = random.randint(0,size-1)
+    y = random.randint(0,size-1)
+    while Array[x,y] != 0:
+        x = random.randint(0,size-1)
+        y = random.randint(0,size-1)
+    return x,y
+
+def snake_fill(B, head, count, s_len):
+
+    if count >= s_len:
+        #pass
+        return count
+
+    if head[0] < 0 or head[0] >= len(B):
+        return count
+    if head[1] < 0 or head[1] >= len(B):
+        return count
+    if B[head[0], head[1]] == 1:
+        return count
+
+    if head[1] - 1 >= 0 and B[head[0], head[1] - 1] == 0:
+        B[head[0], head[1] - 1] = 3
+        count+=1
+
+        rec_count = snake_fill(B, [head[0], head[1] - 1], count, s_len)
+
+        if rec_count>count:
+            count = rec_count
+
+    if head[1] + 1 < len(B) and B[head[0], head[1] + 1] == 0:
+        B[head[0], head[1] + 1] = 3
+        count+=1
+
+        rec_count = snake_fill(B, [head[0], head[1] + 1], count, s_len)
+
+        if rec_count>count:
+            count = rec_count
+
+    if head[0] - 1 >= 0 and B[head[0] - 1, head[1]] == 0:
+        B[head[0] - 1, head[1]] = 3
+        count+=1
+
+        rec_count = snake_fill(B, [head[0] - 1, head[1]], count, s_len)
+
+        if rec_count>count:
+            count = rec_count
+
+    if head[0] + 1 < len(B) and B[head[0] + 1, head[1]] == 0:
+        B[head[0] + 1, head[1]] = 3
+        count+=1
+
+        rec_count = snake_fill(B, [head[0] + 1, head[1]], count, s_len)
+
+        if rec_count>count:
+            count = rec_count
+            
+    return count
+
+############################ Start of main function code #############################################
+
+global SCREEN, CLOCK
+
+#Set the random seed so that each playthrough is identical
+#random.seed(10)
+
+game = False
+user_input = False
+program_over = False
+
+disp_size = 600
+n_boxes = 10
+fps = 10
+border = 1
+
+#Neural Network Details
+#######################################################################################
+
+pop_size = 100
+trials = 5
+generations = 100
+n_inputs = 6
+
+gen = nw.Generation(n_inputs = n_inputs, n_neurons = 512, n_outputs = 4, population_size = pop_size)
+
+#######################################################################################
+
+for generation in range(generations):
+
+    if generation == generations-1:
+        game = True
+
+    if game == True:
+
+        pygame.init()
+
+        SCREEN = pygame.display.set_mode((disp_size,disp_size))
+
+        pygame.display.set_caption("Snake V0")
+
+        CLOCK = pygame.time.Clock()
+
+        #Initial Grid Setup
+        SCREEN.fill(WHITE)
+        box_size = int(disp_size/n_boxes)
+
+    for model in gen.population:
+
+
+        if program_over == False:
+
+            for trial in range(trials):
+
+                #Reset array for update
+                A = numpy.zeros([n_boxes,n_boxes])
+
+                game_over = False
+
+                # #Place head of snake
+                Sx,Sy = random_insert(A, n_boxes)
+                snake = [(Sx,Sy)]
+                A[Sx,Sy] = 1
+                dir = [0,0]
+                grow = False
+                tick = 0
+                food_bonus = 50
+                num_moves = food_bonus
+
+
+                while not game_over and num_moves >= 0:
+
+                    #Remove the addition of food so that we can grow the snake and train it in absence of a food source
+                    #Check to see if Snek food is on the grid
+                    Two_Test = False
+                    for x in range(len(A)):
+                        for y in range(len(A)):
+                            if A[x,y]==2:
+                                Two_Test = True
+
+                    #If there is no food on the grid insert a piece of food at a random location not already filled by snek
+                    if Two_Test!=True:
+                        Ax,Ay = random_insert(A, n_boxes)
+                        A[Ax,Ay]=2
+
+
+                    #Create input array for neural network
+                    #We actually likely don't need any inputs aside from the distance to the foods and the fill information
+                    #B/C the fill info gives some knowledge about wall distance and self distance which are essentially the same thing
+                    
+                    X = numpy.zeros(n_inputs)
+
+                    '''
+                    #Distance to Walls
+
+                    #Distance to left wall
+                    X[0] = snake[0][1]
+                    #Distance to right wall
+                    X[1] = (n_boxes - 1) - X[0]
+                    #Distance to top wall
+                    X[2] = snake[0][0]
+                    #Distance to bottom wall
+                    X[3] = (n_boxes - 1) - X[2]
+                    '''
+
+                    #Distance to food
+
+                    #Distance in the vertical direction
+                    X[0] = Ax - snake[0][0]
+
+                    #Distance in the horizontal direction
+                    X[1] = Ay - snake[0][1]
+
+
+                    '''
+                    #Distance to Self
+                    
+                    #Distance to self left
+                    i = snake[0][1] - 1
+                    self_found = False
+
+                    while i >= 0:
+                        if A[snake[0][0], i] == 1:
+                            X[6] = snake[0][1] - i
+                            self_found = True
+                        i-=1
+                    if self_found == False:
+                        X[6] = snake[0][1]
+
+                    #Distance to self right
+                    i = snake[0][1] + 1
+                    self_found = False
+
+                    while i < n_boxes:
+                        if A[snake[0][0], i] == 1:
+                            X[7] = i - snake[0][1]
+                            self_found = True
+                        i+=1
+                    if self_found == False:
+                        X[7] = (n_boxes - 1) - snake[0][1]
+
+                    #Distance to self up
+                    i = snake[0][0] - 1
+                    self_found = False
+
+                    while i >= 0:
+                        if A[i, snake[0][1]] == 1:
+                            X[8] = snake[0][0] - i
+                            self_found = True
+                        i-=1
+                    if self_found == False:
+                        X[8] = snake[0][0]
+
+                    #Distance to self down
+                    i = snake[0][0] + 1
+                    self_found = False
+
+                    while i < n_boxes:
+                        if A[i, snake[0][1]] == 1:
+                            X[9] = i - snake[0][0]
+                            self_found = True
+                        i+=1
+                    if self_found == False:
+                        X[9] = (n_boxes - 1) - snake[0][0]
+                    '''
+
+                    #Fill function to decide which direction is safest to turn
+
+                    #left fill
+                    B = copy.copy(A)
+                    X[2] = snake_fill(B,(snake[0][0], snake[0][1] - 1), 0, len(snake))
+
+                    #right fill
+                    B = copy.copy(A)
+                    X[3] = snake_fill(B,(snake[0][0], snake[0][1] + 1), 0, len(snake))
+
+                    #up fill
+                    B = copy.copy(A)
+                    X[4] = snake_fill(B,(snake[0][0] - 1, snake[0][1]), 0, len(snake))
+
+                    #down fill
+                    B = copy.copy(A)
+                    X[5] = snake_fill(B,(snake[0][0] + 1, snake[0][1]), 0, len(snake))
+
+
+                    #print(X)
+
+                    #Get prediction from the output of the model being iterated through
+                    output = model.forward(X)
+                    prediction = numpy.argmax(output)
+
+                    if game == True:
+
+                        #Check for game over or user quit
+                        for event in pygame.event.get():
+
+                            #If we want to use user input we take in keyboard inputs
+                            if user_input == True:
+
+                                if event.type == pygame.KEYDOWN:
+                                    if event.key == pygame.K_LEFT and dir != [0,1]:
+                                        dir = [0,-1]
+                                    elif event.key == pygame.K_RIGHT and dir != [0,-1]:
+                                        dir = [0,1]
+                                    elif event.key == pygame.K_UP and dir != [1,0]:
+                                        dir = [-1,0]
+                                    elif event.key == pygame.K_DOWN and dir != [-1,0]: 
+                                        dir = [1,0]
+                                    if event.key == pygame.K_ESCAPE:
+                                        game_over = True
+
+                            if event.type == pygame.QUIT:
+                                game_over = True
+                                program_over = True
+
+                    if user_input == False:
+
+                        if prediction == 0:
+                            if dir == [0,1]:
+                                pass
+                            else:
+                                dir = [0,-1]
+
+                        elif prediction == 1:
+                            if dir == [0,-1]:
+                                pass
+                            else:
+                                dir = [0,1]
+
+                        elif prediction == 2:
+                            if dir == [1,0]:
+                                pass
+                            else:
+                                dir = [-1,0]
+
+                        elif prediction == 3:
+                            if dir == [-1,0]:
+                                pass
+                            else:
+                                dir = [1,0]
+
+
+                    #Do Snake manipulation here 
+
+                    #Check for out of bounds
+                    if snake[0][0]+dir[0] > (n_boxes-1) or snake[0][0]+dir[0] < 0 or snake[0][1]+dir[1] > (n_boxes-1) or snake[0][1]+dir[1] < 0:
+                        game_over = True
+                    
+                    else:
+
+                        #Check for self hit
+                        try:
+                            ind = snake.index((snake[0][0],snake[0][1]),2)
+                            if ind != 0:
+                                game_over = True
+                                print("Game Over Self Hit ", ind)
+                        except ValueError:
+                            pass
+                    
+                    if game_over != True:
+
+                        snake.insert(0,(snake[0][0]+dir[0], snake[0][1]+dir[1]))
+
+                        
+                        if A[snake[0][0],snake[0][1]] == 2:
+                             grow = True
+                             num_moves += food_bonus
+
+                        if grow != True:
+                            A[snake[-1][0],snake[-1][1]]=0
+                            snake.pop(-1)
+                        else:
+                            grow = False
+
+                        for block in snake:
+                            A[block[0],block[1]]=1
+
+                        if game == True:
+
+                            for x in range(len(A)):
+                                for y in range(len(A)):
+                                    if A[x,y] == 0:
+                                        draw_rect(y,x,BLACK,box_size,border)
+                                    if A[x,y] == 1:
+                                        draw_rect(y,x,BLUE,box_size,border)
+                                    if A[x,y] == 2:
+                                        draw_rect(y,x,GREEN,box_size,border)
+                            CLOCK.tick(fps)
+                            pygame.display.update()
+
+                        tick += 1
+                        num_moves -= 1
+
+                #Once we've broken out of the loop quit the game
+                score = tick*(len(snake)-1)
+
+                model.score += score
+
+            model.score = model.score/trials
+
+    gen.score_sort()
+
+
+    print("Gen ", generation, " Score is: ", gen.population[0].score)
+
+    gen.generation_mate()
+
+
+pygame.quit()
+quit()
+
+
+
+
+
+
+
+
+
